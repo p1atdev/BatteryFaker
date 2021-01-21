68c82c514ebe0b20ceb810ab36bb7fc38d9a9c24
Shu Teiei <shu_teiei@outlook.jp>
2021-01-21 22:21:41 +0900

    Initial Commit

---
+++ BatteryFaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BatteryFaker
+//
+//  Created by 周廷叡 on 2021/01/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BatteryFaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BatteryFaker
+//
+//  Created by 周廷叡 on 2021/01/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
