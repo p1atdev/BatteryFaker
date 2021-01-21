5798ba97c3f927e8bcdcb7763491b781cd209448
Shu Teiei <shu_teiei@outlook.jp>
2021-01-21 22:39:53 +0900

    feat: 作った()

--- BatteryFaker/AppDelegate.swift
+++ BatteryFaker/AppDelegate.swift
@@ -11,10 +11,27 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
+    //例のメニュー
+    @IBOutlet weak var menu: NSMenu!
+    
+    //メニューバーに表示されるアプリケーションを作成
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
-
+    //これは起動時に実行されるやつだよ
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        //画像を取ってくるよ
+        let batteryImage: NSImage = NSImage(named: "battery")!  //この"!"は絶対にnilが入らないよ!っていう保証みたいなやつ
+        
+        // メニューバーに表示される文字列
+        self.statusItem.title = "0%"
+        //MARK: 画像
+        self.statusItem.image = batteryImage
+        //ハイライトモード
+        self.statusItem.highlightMode = false
+        //メニュー
+        self.statusItem.menu = menu
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
